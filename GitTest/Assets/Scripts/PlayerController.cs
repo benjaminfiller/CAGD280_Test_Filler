b3fdfc96fcae7b2f73b67c6a83b76f2b82113a96
benjaminfiller <130104517+benjaminfiller@users.noreply.github.com>
2023-10-31 13:24:40 -0700

    Fixed bug for jumping

--- GitTest/Assets/Scripts/PlayerController.cs
+++ GitTest/Assets/Scripts/PlayerController.cs
@@ -5,4 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Implement player movement
+    public float speed;
+    private Vector3 movePos;
+
+    private void MovePlayer()
+    {
+
+    }
+
+    private void Jump()
+    {
+        //makes the player Jump
+    }
 }
